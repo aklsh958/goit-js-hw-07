f00efb23e985571524378f4fcd45de71cd4c2b39
Artem Kulish <akulish247@gmail.com>
2025-05-21 19:12:10 +0200

    update

---
+++ js/task-4.js
@@ -0,0 +1,18 @@
+const formEl = document.querySelector('.js-form');
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const email = formEl.elements.email.value.trim();
+  const password = formEl.elements.password.value.trim();
+
+  
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+  const userData = { email, password };
+  console.log(userData);
+ 
+  e.target.reset();
+});
